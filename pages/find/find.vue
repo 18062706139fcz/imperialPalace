74bb917d57b33fda5952483b187eadf991079f17
KiwiBerry <1826856012@qq.com>
2023-01-05 17:28:25 +0800

    mine页面UI完成

--- pages/find/find.vue
+++ pages/find/find.vue
@@ -2,7 +2,7 @@
 	<view class="find_page">
 		<view class="find_swiper">
 			<uni-swiper-dot :info="info" :current="current" field="content" mode="dot" :dotsStyles="dotsStyles">
-				<swiper class="swiper-box" @change="change" autoplay="true">
+				<swiper class="swiper-box" @change="change" autoplay="true" circular="true">
 					<swiper-item v-for="(item ,index) in info" :key="item.id">
 						<view class="swiper-item">
 							{{item.content}}
@@ -16,22 +16,22 @@
 			<view class="find_box_herf">
 				<view class="find_box_herf_contents" v-for="(item) in find_box_herf_contents" :key="item">
 					<navigator url="#">
-						<text> {{item}}</text>
+						<text style="font-weight: 600 !important; font-family:‘Courier New’, Courier, monospace;"> {{item}}</text>
 					</navigator>
 
 				</view>
 			</view>
 			<view class="cultural_guided_tour">
 				<view class="cultural_guided_tour_text">
-					<text>文化导览</text>
-					<text>全部 ＞</text>
+					<text style="font-family:‘Courier New’, Courier, monospace;">文化导览</text>
+					<text style="font-size: 12px; font-weight: 550 !important; font-family:‘Franklin Gothic Medium’, ‘Arial Narrow’, Arial, sans-serif;">全部 ＞</text>
 				</view>
 				<scroll-view class="cultural_guided_tour_img" scroll-x="true">
 					<view class="cultural_guided_tour_img_view">
 						<view class="cultural_guided_tour_img_li" v-for="item in cultural_guided_tour_img"
 							:key="item.id">
 							<image :src="item.src" style="height: 120px; width: 120px; border-radius: 10px;"></image>
-							<text style="display: block;">{{item.text}}</text>
+							<text style="font-size: 13px; font-weight: 550 !important; display: block;  font-family:‘Franklin Gothic Medium’, ‘Arial Narrow’, Arial, sans-serif;">{{item.text}}</text>
 						</view>
 					</view>
 				</scroll-view>
@@ -39,13 +39,13 @@
 			</view>
 			<view class="cultural_collage">
 				<view class="cultural_collage_text">
-					<text>文化拼贴</text>
-					<text>全部 ＞</text>
+					<text style="font-weight: 550 !important; font-family:‘Courier New’, Courier, monospace;">文化拼贴</text>
+					<text style="font-size: 12px; font-weight: 550 !important; font-family:‘Franklin Gothic Medium’, ‘Arial Narrow’, Arial, sans-serif;">全部 ＞</text>
 				</view>
 				<image src="../../static/logo.png" style="height: 120px; width: 275px;border-radius: 10px;"></image>
 			</view>
 			<view class="cultural_imprint" style="margin-top: 30px;">
-				<text style="font-weight: 550; ">身边的文化印记</text>
+				<text style="font-weight: 550 !important; font-family:‘Courier New’, Courier, monospace;">身边的文化印记</text>
 				<view class="cultural_imprint_img" style="display: flex; padding-bottom: 50px;">
 					<image src="../../static/logo.png"
 						style="height: 150px; width: 135px;border-radius: 10px; padding-right: 15px;"></image>
@@ -127,29 +127,29 @@
 		.find_swiper {
 			height: auto;
 			background-color: #853e38;
-			position: relative;
-
+			position: absolute;
+			left: 0;
+			top: 0;
+			z-index: -999;
+			width: 100%;
+			height: 170px;
 			.swiper-box {
-
-				// left: 0;
-				// top: 0;
-
-				.swiper-item {
-					color: #ffffff;
-				}
 			}
 
 			.find_swiper_text {
 				position: absolute;
-				bottom: 25px;
+				bottom: 45px;
 				left: 8px;
 				color: #ffffff;
-				// font-family: xiaosong;
+				font-family:‘Courier New’, Courier, monospace;;
 			}
 		}
 
 		.find_box {
-			// border-radius: 20px 20px 0 0;
+			border-radius: 20px 20px 0 0;
+			// position: absolute;
+			width: 100%;
+			margin-top: 150px;
 			height: auto;
 			background-color: #f6f2e6;
 			box-sizing: border-box;
@@ -166,9 +166,9 @@
 				// align-items: center;
 				.find_box_herf_contents {
 					font-size: 11px;
-					font-weight: 600;
+					// font-weight: 600;
 					// padding-left: 5px;
-					padding-right: 16px;
+					padding-right: 14px;
 				}
 
 			}
@@ -179,7 +179,7 @@
 				.cultural_guided_tour_text {
 					display: flex;
 					justify-content: space-between;
-					font-weight: 550;
+					
 				}
 
 				.cultural_guided_tour_img {
@@ -192,7 +192,7 @@
 
 						.cultural_guided_tour_img_li {
 							padding-right: 10px;
-							font-weight: 550;
+							
 
 						}
 					}
@@ -207,7 +207,7 @@
 				.cultural_collage_text {
 					display: flex;
 					justify-content: space-between;
-					font-weight: 550;
+					
 				}
 			}
 		}
